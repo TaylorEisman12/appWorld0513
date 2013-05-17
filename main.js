8b7927c0e69d482d9c2471ae829e30bb8acb894e
Taylor Eisman <TaylorEisman@fullsail.edu>
2013-05-17 01:29:34 -0500

    got csv to display data working on style

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $('#loginButton').on('click', function(){
 });
 
 $('.index').on('pageinit', function(){
+	$('<li><a href="#remoteData" data-role="button" id="viewRemoteDataPage">Remote Data Page</a></li>').appendTo('#buttonList');
 	$('<li data-role="list-divider">View Saved Buttons by Theme</li>').appendTo('#buttonList');
 	$('<li><a href="#" data-role="button" id="themeA">A themed Buttons</a></li>').appendTo('#buttonList');
 	$('<li><a href="#" data-role="button" id="themeB">B themed Buttons</a></li>').appendTo('#buttonList');
@@ -81,3 +82,97 @@ $('#logOut').on('click', function(){
 	alert('You have been Logged Out.');
 	$.mobile.changePage($('#login'));
 });
+
+
+
+$( '#remoteData' ).on('pageinit', function(){
+		$( '#jsonButton' ).on( 'click', function () {
+		$('#viewData').empty();
+        $.ajax( {
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success:function ( result ) {
+	//console.log(result);
+                for ( var i = 0, len = result.armors.length; i < len; i++ ) {
+                    var item = result.armors[i];
+	console.log(item);
+                    $( ' ' +
+						'<div class="contentJSON">' +
+							'<ul>' +
+								'<li>' + '<b>' + item.armorName[0] + " " + item.armorName[1] + '</b>' + '</li>' +
+								'<li>' + item.selectColor[0] + " " + item.selectColor[1] + '</li>' +
+								'<li>' + item.repaired[0]	+ " " + item.repaired[1] + '</li>' +
+							'</ul>' +
+						'</div>'
+					).appendTo( '#viewData' );
+					
+                }
+            }
+        });
+		return false;
+    });
+
+	$( '#xmlButton' ).on( 'click', function() {
+		$('#viewData').empty();
+			$.ajax( {
+				url: 'xhr/data.xml',
+				type: 'GET',
+				dataType: 'xml',
+				success:function ( result ) {
+				//console.log(result);
+				$(result).find('item').each(function(){
+					var armorName = $(this).find('armorName').text();
+                    var selectColor = $(this).find('selectColor').text();
+                    var repaired = $(this).find('repaired').text();
+                        $(' '+
+                            '<div class="contentXML">' +
+                                '<ul>' +
+                                    '<li><b>Armor Name: ' + armorName + '</b></li>' +
+                                    '<li> Color: ' + selectColor + '</li>' +
+                                    '<li> Repaired: ' + repaired + '</li>' +
+                                '</ul>' +
+                            '</div>'
+						).appendTo('#viewData');
+						
+					});
+				}
+			});
+			return false;
+    });
+
+
+$( '#csvButton' ).on( 'click', function() {
+$('#viewData').empty();
+        $.ajax( {
+            url: 'xhr/data.csv',
+            type: 'GET',
+            dataType: 'text',
+            success:function ( result ) {
+			//console.log(result);
+			var lines = result.split("\n");
+			//console.log(lines);
+			var dataRow = lines[0];
+			var dataCol = dataRow.split(",");
+			for (var lineNum = 1; lineNum < lines.length; lineNum++) {
+				var row = lines[lineNum];
+				var columns = row.split(",");
+				//console.log(columns);
+			$(''+
+		'<div class="contentCSV">'+
+			'<ul>' +
+				'<li><b>' + dataCol[0] + " " + columns[0] + '</b></li>' +
+				'<li>'+ dataCol[1] + " " + columns[1] + '</li>' +
+				'<li>'+ dataCol[2] + " " + columns[2] + '</li>' +
+				'<li>'+ dataCol[3] + " " + columns[3] + '</li>' +
+			'</ul>' +
+		'</div>'
+				).appendTo('#viewData');
+				
+		}
+            }
+        });
+    });
+return false;
+});
+
